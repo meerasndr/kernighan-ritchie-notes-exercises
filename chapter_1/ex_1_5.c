d258ddf7c804fe243bcc1f7f727e28948d67df18
Meera Sundar <mirage.sundial@gmail.com>
2020-09-18 10:07:15 +0530

    chapter 1

--- chapter_1/ex_1_5.c
+++ chapter_1/ex_1_5.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#define LOWER 0.0
+#define UPPER 300.0
+#define STEP 20
 int main(){
     float celsius, fahrenheit;
-    int step, lower, upper;
-    step = 20;
-    lower = 0.0;
-    upper = 300.0;
     printf("Fahrenheit\tCelsius\n");
-    for(fahrenheit = upper; fahrenheit >= lower; fahrenheit = fahrenheit-step){
+    for(fahrenheit = UPPER; fahrenheit >= LOWER; fahrenheit = fahrenheit-STEP){
         celsius = 5.0 * (fahrenheit - 32.0) / 9.0;      
         printf("%3.0f\t\t%6.1f\n", fahrenheit, celsius);
     }
