91e4c70b1569664934a1f9247eb3603f25951351
Meera Sundar <mirage.sundial@gmail.com>
2020-11-03 14:32:39 +0530

    clean up gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# kernighan-ritchie-notes-exercises
-#### My notes & attempts at exercises from the K&R C book
--- chapter_1/ex_1_7.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-// Write a program to print the value of EOF
-
-int main(){
-    int c;
-    printf("Value of EOF is %d: \n", EOF); // This prints -1 (not 0)!!
-    return 0;
-}
