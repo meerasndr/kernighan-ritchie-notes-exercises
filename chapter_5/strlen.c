5cfcd22a2a214440b89f4fba5c6ce4e50fae17a1
Meera Sundar <mirage.sundial@gmail.com>
2021-02-03 11:33:26 +0530

    modify strlen

--- chapter_5/strlen.c
+++ chapter_5/strlen.c
@@ -3,8 +3,8 @@
 int mystrlen(char *localstring){
     int length;
     for(length = 0; *localstring != '\0'; localstring++){
-        printf("The current character is %c\n", *localstring);
-        printf("The current character's address is %p\n", localstring);
+        //printf("The current character is %c\n", *localstring);
+        //printf("The current character's address is %p\n", localstring);
         length++;
     }
     return length;
@@ -12,5 +12,8 @@ int mystrlen(char *localstring){
 
 int main(void){
     char *string = "hello world";
+    char stringarr[] = "good morning!!";
     printf("The length of the string is %d\n", mystrlen(string));
+    printf("The length of the string is %d\n", mystrlen(stringarr));
+    printf("The length of the string is %d\n", mystrlen("Want to say hi there"));
 }
